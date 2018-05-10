ac63b6167659d741c4f549de34edf5fa8bb861cb
linceln <lince2030@gmail.com>
2018-05-11 07:43:44 +0800

    GLSurfaceView

--- app/src/main/java/cc/lince/camera/MainActivity.java
+++ app/src/main/java/cc/lince/camera/MainActivity.java
@@ -2,56 +2,40 @@ package cc.lince.camera;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 
-import cc.lince.camera.util.FileUtils;
-import cc.lince.camera.util.PhotoUtils;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private static final int REQUEST_CODE_CAMERA_PERMISSION = 1;
     private ImageView mIvCapture;
+    private GLSurfaceView glSurfaceView;
+    private SurfaceTexture surfaceTexture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mIvCapture = findViewById(R.id.ivCapture);
-        mIvCapture.setOnClickListener(new View.OnClickListener() {
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        glSurfaceView.setRenderer(new GLRenderer(glSurfaceView, new GLRenderer.OnSurfaceTextureListener() {
             @Override
-            public void onClick(View v) {
-//                capture();
-                CameraHelper.getInstance().capture(MainActivity.this, new Camera.PictureCallback() {
-                    @Override
-                    public void onPictureTaken(byte[] data, Camera camera) {
-                        camera.startPreview();
-                        Log.e("camera", "onPictureTaken");
-                        String s = FileUtils.savePicture(MainActivity.this, data);
-                        Log.e("camera", "onPictureTaken: " + s);
-                        Bitmap bgBitmap = BitmapFactory.decodeFile(s);
-                        Bitmap fgBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.capture);
-                        Bitmap bitmap = PhotoUtils.compose(bgBitmap, fgBitmap, 0, 0);
-                        FileUtils.savePicture(bitmap);
-                        mIvCapture.setImageBitmap(bitmap);
-                    }
-                });
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture) {
+                MainActivity.this.surfaceTexture = surfaceTexture;
             }
-        });
-
-        int numberOfCameras = Camera.getNumberOfCameras();
-        Log.d("mCamera", "Camera numbers: " + numberOfCameras);
-        // TODO 判断有没有相机以及能不能同时打开前后摄像头
+        }));
+        setContentView(glSurfaceView);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -60,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                             Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_CODE_CAMERA_PERMISSION);
         } else {
-            initView();
+            openCamera();
         }
     }
 
@@ -69,15 +53,22 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initView();
+                openCamera();
             }
         }
     }
 
-    private void initView() {
-        CustomSurfaceView backSurfaceView = findViewById(R.id.backSurfaceView);
-        backSurfaceView.setCameraId(0);
-        CustomSurfaceView frontSurfaceView = findViewById(R.id.frontSurfaceView);
-        frontSurfaceView.setCameraId(1);
+    private void openCamera() {
+        if (surfaceTexture != null) {
+            Camera camera = Camera.open();
+            try {
+                camera.setPreviewTexture(surfaceTexture);
+                camera.startPreview();
+            } catch (IOException e) {
+                Log.e(TAG, "setPreviewTexture exception");
+            }
+        } else {
+            Log.e(TAG, "Open camera failed: SurfaceTexture not ready");
+        }
     }
 }
