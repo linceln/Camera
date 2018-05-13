1c74c3a7a97c6d0bfbdc4dda74005dee54bf26bd
linceln <lince2030@gmail.com>
2018-05-13 09:41:40 +0800

    GLSurfaceView for previewing

---
+++ app/src/main/java/cc/lince/camera/CameraGLSurfaceView.java
@@ -0,0 +1,158 @@
+package cc.lince.camera;
+
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import java.io.IOException;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CameraGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Renderer {
+
+    private static final String TAG = "CameraGLSurfaceView";
+
+    private SurfaceTexture mSurfaceTexture;
+    private DirectDrawer mDirectDrawer;
+    private OnSurfaceCreatedListener mListener;
+    private Camera mCamera;
+
+    public CameraGLSurfaceView(Context context) {
+        this(context, null);
+    }
+
+    public CameraGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        this.setEGLContextClientVersion(2);
+        this.setRenderer(this);
+        this.setRenderMode(RENDERMODE_WHEN_DIRTY);
+    }
+
+    public void setOnSurfaceCreatedListener(OnSurfaceCreatedListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Log.e(TAG, "Renderer onSurfaceCreated START. Thread" + Thread.currentThread().getName());
+        int[] textureId = new int[1];
+        // 生成一个纹理
+        GLES20.glGenTextures(textureId.length, textureId, 0);
+        // 将此纹理绑定到 OES 外部纹理上
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES, textureId[0]);
+        // 设置纹理过滤参数
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+        // 解除纹理绑定
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_BINDING_EXTERNAL_OES, 0);
+
+        // 创建一个 SurfaceTexture 绑定到 openGL 外部 OES 纹理
+        mSurfaceTexture = new SurfaceTexture(textureId[0]);
+        mDirectDrawer = new DirectDrawer(textureId[0]);
+        mSurfaceTexture.setOnFrameAvailableListener(new SurfaceTexture.OnFrameAvailableListener() {
+            @Override
+            public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+                requestRender();
+                Log.e(TAG, "SurfaceTexture onFrameAvailable and requestRender " + Thread.currentThread().getName());
+            }
+        });
+
+        if (mListener != null) {
+            mListener.onSurfaceCreated(mSurfaceTexture);
+        }
+        Log.e(TAG, "Renderer onSurfaceCreated END");
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        Log.e(TAG, "Render onSurfaceChanged width: " + width + " height: " + height);
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        Log.e(TAG, "Renderer onDrawFrame START. Thread:" + Thread.currentThread().getName());
+        GLES20.glClearColor(1f, 1f, 1f, 1f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        // 更新纹理图像
+        mSurfaceTexture.updateTexImage();
+        Log.e(TAG, "Renderer onDrawFrame: Texture timestamp: " + mSurfaceTexture.getTimestamp());
+        float[] matrix = new float[16];
+        // 获取外部纹理的变换矩阵
+        mSurfaceTexture.getTransformMatrix(matrix);
+        // TODO Shader
+        mDirectDrawer.draw(matrix);
+        Log.e(TAG, "Renderer onDrawFrame END");
+    }
+
+    public void stopPreview() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+        }
+    }
+
+    public void startPreview() {
+        if (mSurfaceTexture != null) {
+            mCamera = Camera.open();
+            try {
+                mCamera.setPreviewTexture(mSurfaceTexture);
+                mCamera.addCallbackBuffer(allocBuffer());
+                mCamera.addCallbackBuffer(allocBuffer());
+                mCamera.addCallbackBuffer(allocBuffer());
+                mCamera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
+                    @Override
+                    public void onPreviewFrame(byte[] data, Camera camera) {
+                        // 在 SurfaceTexture 的 OnFrameAvailable 之后回调
+                        Log.e(TAG, "Camera onPreviewFrame callback: data length:" + data.length
+                                + ". Thread: " + Thread.currentThread().getName());
+                        camera.addCallbackBuffer(data);
+                    }
+                });
+                mCamera.startPreview();
+                Log.e(TAG, "Camera start previewing");
+            } catch (IOException e) {
+                Log.e(TAG, "setPreviewTexture exception");
+            }
+        } else {
+            Log.e(TAG, "Open camera failed: SurfaceTexture not ready");
+        }
+    }
+
+    /**
+     * 根据预览格式和预览尺寸获得缓冲
+     *
+     * @return 缓冲
+     */
+    private byte[] allocBuffer() {
+        Camera.Parameters parameters = mCamera.getParameters();
+        Camera.Size previewSize = parameters.getPreviewSize();
+        Log.e(TAG, "Camera previewSize: " + previewSize.width + "x" + previewSize.height);
+        int previewFormat = parameters.getPreviewFormat();
+        Log.e(TAG, "Camera previewFormat: " + previewFormat);
+        int bufferSize = previewSize.width * previewSize.height
+                * ImageFormat.getBitsPerPixel(previewFormat) / 8;
+        Log.e(TAG, "Camera allocBuffer: " + bufferSize);
+        return new byte[bufferSize];
+    }
+
+    public interface OnSurfaceCreatedListener {
+        void onSurfaceCreated(SurfaceTexture surfaceTexture);
+    }
+}
--- app/src/main/java/cc/lince/camera/MainActivity.java
+++ app/src/main/java/cc/lince/camera/MainActivity.java
@@ -3,48 +3,42 @@ package cc.lince.camera;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
-import android.hardware.Camera;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private static final int REQUEST_CODE_CAMERA_PERMISSION = 1;
     private ImageView mIvCapture;
-    private GLSurfaceView glSurfaceView;
-    private SurfaceTexture surfaceTexture;
+    private CameraGLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setEGLContextClientVersion(2);
-        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        glSurfaceView.setRenderer(new GLRenderer(glSurfaceView, new GLRenderer.OnSurfaceTextureListener() {
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture) {
-                MainActivity.this.surfaceTexture = surfaceTexture;
-            }
-        }));
+        glSurfaceView = new CameraGLSurfaceView(this);
         setContentView(glSurfaceView);
+        glSurfaceView.setOnSurfaceCreatedListener(new CameraGLSurfaceView.OnSurfaceCreatedListener() {
+            @Override
+            public void onSurfaceCreated(SurfaceTexture surfaceTexture) {
+                requestPermissionOrOpenCamera();
+            }
+        });
+    }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
+    private void requestPermissionOrOpenCamera() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.CAMERA,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_CODE_CAMERA_PERMISSION);
         } else {
-            openCamera();
+            glSurfaceView.startPreview();
         }
     }
 
@@ -53,22 +47,21 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera();
+                glSurfaceView.startPreview();
             }
         }
     }
 
-    private void openCamera() {
-        if (surfaceTexture != null) {
-            Camera camera = Camera.open();
-            try {
-                camera.setPreviewTexture(surfaceTexture);
-                camera.startPreview();
-            } catch (IOException e) {
-                Log.e(TAG, "setPreviewTexture exception");
-            }
-        } else {
-            Log.e(TAG, "Open camera failed: SurfaceTexture not ready");
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+        glSurfaceView.stopPreview();
     }
 }
